61d5ea0b9ba429bbf4a9cd47ad07a9729d90acbf
Alexandre Mulatinho <alex@mulatinho.net>
2016-09-03 19:39:15 -0300

    adding new commands...

--- src/utils.h
+++ src/utils.h
@@ -3,22 +3,34 @@
 #ifndef _ZTUTILS_H_
 #define _ZTUTILS_H_
 
-#define GROUP_OWNERS  0
-#define GROUP_PRIVATE 1
-#define GROUP_PUBLIC  2
+#define GROUP_MYSELF  0
+#define GROUP_OWNER   1
+#define GROUP_PRIVATE 2
+#define GROUP_PUBLIC  4
 
 #define GOOGLE "www.google.com"
+
+enum { PRIVMSG, ACTION, PING, GLOBAL };
+char *type_msg[] = { "PRIVMSG", "ACTION", "PING", "GLOBAL" };
 
 typedef struct
 {
 	char command[BUF_MIN];
 
-	int group;
+	int type_message, group;
+
+	int (*func)(zt_info *, char *);
 } zt_commands_t;
 
-static zt_commands_t zt_cmd[] = {
-	{"!google", GROUP_PUBLIC }
+int zt_cmd_google(zt_info *, char *);
+int zt_cmd_quote(zt_info *, char *);
+int zt_cmd_pong(zt_info *, char *);
 
+static zt_commands_t zt_cmd[] = {
+	{"!google", 	PRIVMSG, 	GROUP_PUBLIC,	&zt_cmd_google,	},
+	{"!quote", 	PRIVMSG, 	GROUP_PRIVATE,	&zt_cmd_quote,	},
+	{"PING :", 	PING, 		GROUP_PUBLIC,	&zt_cmd_pong,	}
 };
+static int zt_commands_sz = sizeof(zt_cmd) / sizeof(zt_cmd[0]);
 
 #endif
