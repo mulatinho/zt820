1cb04da3be84c5c8024422a3d8a6792862f0e7b9
Alexandre Mulatinho <alex@mulatinho.net>
2016-09-04 13:05:33 -0300

    change nanosleep

--- src/main.c
+++ src/main.c
@@ -140,7 +140,7 @@ int zt_create_server(zt_info *ztinfo)
 
 	fprintf(stderr, ":. server going to wait events..\n");
 	while (1) {
-		ret = poll(serverpoll, 1, 2500);
+		ret = poll(serverpoll, 1, 25000);
 		if (ret < 0) {
 			fprintf(stderr, "poll: requested event failed!\n");
 			return -2;
@@ -186,7 +186,7 @@ int zt_create_server(zt_info *ztinfo)
 			memset(buf, '\0', sizeof(buf));
 			rbytes = 0;
 		}
-		usleep(5000);
+		usleep(500000);
 	}
 	
 	return 0;
