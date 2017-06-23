5fb7385b9c5c8f99d114d33df1d8e0c431c126eb
Alexandre Mulatinho <alex@mulatinho.net>
2017-06-23 10:53:46 -0300

    add our "first" test code without testing :P

--- tests/test.c
+++ tests/test.c
@@ -1,75 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define BUF_MIN 64
-#define BUF_MED 256
-#define BUF_MAX 1024
-
-typedef struct data_msg {
-    int id;
-
-    char nick[BUF_MIN];
-
-    char host[BUF_MED];
-
-    char command[BUF_MIN];
-
-    char argument[BUF_MED];
-
-    char message[BUF_MAX];
-} zt_data;
-
-void zt_get_data(zt_data *data, const char *buffer)
-{
-    struct data_msg payload = {
-        .id = 0, .nick = {0},
-        .host = {0}, .command = {0}, .argument = {0}, .message = {0}
-    };
-    int dots = 0, sep = 0, n = 0;
-
-    for (int i = 0; i < strlen(buffer); i++) {
-        if (' ' == buffer[i] && dots < 2) { sep++; n = 0; continue; }
-
-        if (dots == 2) { payload.message[n++] = buffer[i]; }
-        else {
-            switch (sep) {
-                case 1:
-                    if ('!' == buffer[i]) { sep++; n = 0; continue; }
-                    else { payload.nick[n++] = buffer[i]; }
-                    break;
-                case 2: payload.host[n++] = buffer[i]; break;
-                case 3: payload.command[n++] = buffer[i]; break;
-                case 4: payload.argument[n++] = buffer[i]; break;
-                default: break;
-            }
-        }
-
-        if (':' == buffer[i]) { dots++; sep++; n = 0; }
-    }
-
-	n = 0; sep = 0;
-	//fprintf(stdout, "%s = %d\n", payload.nick, strlen(payload.nick));
-	if (1 == strlen(payload.nick)) {
-		strcpy(payload.nick, "IRCSERVER");
-
-    	for (int i = 0; i < strlen(buffer); i++) {
-			if (' ' == buffer[i] || ':' == buffer[i]) { sep++; n = 0; continue; }
-			if (!sep) { payload.command[n++] = buffer[i]; }
-			else { payload.host[n++] = buffer[i]; }
-		}
-	}
-
-    zt_clean_string(payload.nick);
-	zt_clean_string(payload.host);
-	zt_clean_string(payload.command);
-	zt_clean_string(payload.argument);
-	zt_clean_string(payload.message);
-
-	*data = payload;
-    fprintf(stdout, "nick: '%s', host: '%s'\ncommand: '%s', argument: '%s', message: '%s'\n",
-        payload.nick, payload.host, payload.command, payload.argument, payload.message);
-}
+#include "../src/main.h"
 
 int main(int argc, char **argv)
 {
